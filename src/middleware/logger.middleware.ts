d211bbee3dc37d2efc43f66e8035eca56d1e460a
지토지토 <iam@zito.kr>
2025-12-12 02:28:02 +0900

    feat: 미들웨어 추가 (클래스형, 함수형)

---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware {
+  use(req, res, next) {
+    console.log('Request...');
+    next();
+  }
+}
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  console.log('function Reqeust....');
+  console.log(req.headers);
+  next();
+}
