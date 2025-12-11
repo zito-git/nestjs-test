edbf2ee6db649f12ef32cda0d534289434b5aae3
지토지토 <iam@zito.kr>
2025-12-12 02:28:54 +0900

    feat: implements NestModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { logger, LoggerMiddleware } from './middleware/logger.middleware';
 import { UsersModule } from './users/users.module';
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger).forRoutes('*');
+  }
+}
